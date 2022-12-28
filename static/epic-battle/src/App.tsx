c01ac5e10bb6df46bcd7364b28f228195287f6bc
Edwin Dijas Chiwona <c-edwin.chiwona@on24.com>
2022-12-28 15:34:13 +0200

    EBX-15 Add React Intl

--- static/epic-battle/src/App.tsx
+++ static/epic-battle/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { IntlProvider } from 'react-intl';
 import { Router } from './router/Router';
+import defaultMessages from 'lang/en.json'
 
 function App() {
   return (
     <div data-testid='main-app' className="App">
-      My Game coming soon
-      <Router />
+      <IntlProvider locale='en' messages={defaultMessages} >
+        <Router />
+      </IntlProvider>
     </div>
   );
 }
