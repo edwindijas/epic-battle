852eb41e80b036d64e59f50c5757c65d83973c52
Edwin Chiwona <edwinchiwona+git@gmail.com>
2023-01-15 17:10:30 +0200

    EBX-65 Fix Leaderboard UI

---
+++ game/src/mock/generateMockLeaderBoard.js
@@ -0,0 +1,36 @@
+const randomNames = [];
+const fs = require('fs');
+const path = require('path')
+const baseUser = require(path.join(__dirname, './User.json'));
+const {v4: uuid} = require('uuid');
+const { produce } = require('immer');
+
+const names = require(path.join(__dirname, './firstname.json'));
+const fnames = require(path.join(__dirname, './lastname.json'));
+
+
+const r = () => {
+    const fIndex = Math.floor(Math.random() * fnames.length);
+    const lIndex = Math.floor(Math.random() * names.length);
+    return `${fnames[fIndex]} ${fnames[lIndex]}`
+}
+
+const users = [];
+for (let x = 0; x < 1000; x++) {
+    const id = uuid();
+    const highscore = Math.floor(Math.random() * 100000);
+    const user = produce(baseUser, (draft) => {
+        draft.accountId = id;
+        draft.displayName = r();
+        draft.highscore = highscore;
+        return draft;
+    })
+
+    users.push(user);
+}
+
+
+users.sort((a, b) => b.highscore - a.highscore);
+
+fs.writeFileSync(path.join(__dirname, './user.data.json'), JSON.stringify(users, null, ' '));
+
---
+++ game/src/models/LeaderBoard.ts
@@ -0,0 +1,20 @@
+//import { requestJira } from '@forge/bridge';
+import { requestJira } from './bridge';
+import { User } from './types';
+import MockUsers from "mock/user.data.json";
+
+const BASE_URL = '/rest/api/3/'
+
+const URL_CURRENT_USER = BASE_URL + 'myself'
+
+export const getUser = async () => {
+    return await requestJira(URL_CURRENT_USER).then(
+        (response: any) => {
+            return response.json() as unknown
+        }
+    ).then((response) => {
+       return response as User[];
+    }).catch((e) => {
+       return MockUsers;
+    })
+}
